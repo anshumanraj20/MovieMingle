f1c70e48fdef5e235436918087a25fd6182a8b46
Anshuman Raj <anshumanraj2003@gmail.com>
2024-10-11 11:52:50 +0530

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Movielist from './Components/Movielist/Movielist'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      
+    </div>
+    
   )
 }
 
